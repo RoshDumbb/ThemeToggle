9eaaa2e8358398ed41c56d0dba0fa1a8548ad696
RoshDumbb <roshanidumbhare30@gmail.com>
2022-05-23 09:38:22 +0530

    created react app

---
+++ toggle/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Auth0ProviderWithHistory from './Components/Authentication';
+
+function App() {
+  return (
+    <div className="App">
+      <Auth0ProviderWithHistory/>
+    </div>
+  );
+}
+
+export default App;
